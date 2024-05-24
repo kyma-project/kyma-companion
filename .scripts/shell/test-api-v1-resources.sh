1ef03d9297769e6d0072eda5734e22e1b071d33e
Tibor Kiss <tibor.kiss01@sap.com>
2024-05-24 15:11:41 +0200

    Compress code

---
+++ .scripts/shell/test-api-v1-resources.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+# Test - API v1 resources: GET /api/v1/resources
+{ # try
+
+    curl --request GET --url "http://localhost:32000/api/v1/resources" --header "Content-Type: application/json"
+    #save your output
+
+} || { # catch
+    POD=$(kubectl get pod -n ai-core -l app=ai-backend -o jsonpath="{.items[0].metadata.name}")
+    kubectl describe pod $POD -n ai-core
+    kubectl logs $POD -n ai-core
+}
