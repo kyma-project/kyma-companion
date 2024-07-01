dd8e517de863acd7c82ec5435621709b06f347df
Mansur Uralov <m.uralov@sap.com>
2024-07-01 16:35:11 +0200

    Improve for Review Comments - Add service layer - Improve async function usage - Use async testing

--- src/routes/api.py
+++ src/routes/api.py
@@ -1,36 +1,20 @@
 from fastapi import FastAPI
 
 from src.utils.logging import get_logger
-from src.utils.models import get_model
+from src.services.chat import init_chat, process_chat_request
 
 logger = get_logger(__name__)
 
 app = FastAPI()
 
 
-def init_chat() -> dict:
-    """ Initialize the chat """
-    logger.info("Initializing chat...")
-    return {"message": "Chat is initialized!"}
-
-
-def chatting() -> dict:
-    """ Chat with the Kyma companion """
-    logger.info("Processing request...")
-
-    llm = get_model("gpt-4o")
-    logger.info(f"LLM model {llm} is created.")
-
-    return {"message": "Hello!"}
-
-
 @app.get("/chat/init")
 async def init() -> dict:
     """ Endpoint to initialize the chat with the Kyma companion """
-    return init_chat()
+    return await init_chat()
 
 
 @app.get("/chat")
 async def chat() -> dict:
     """ Endpoint to chat with the Kyma companion """
-    return chatting()
+    return await process_chat_request()
--- tests/routes/test_api.py
+++
@@ -1,9 +0,0 @@
-from src.routes.api import chatting, init_chat
-
-
-def test_init_chat():
-    assert init_chat() == {"message": "Chat is initialized!"}
-
-
-def test_chatting():
-    assert chatting() == {"message": "Hello!"}
