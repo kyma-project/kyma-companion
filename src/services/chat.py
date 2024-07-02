f92e77921a5048c6411ce4eece5c19da994336a6
Mansur Uralov <m.uralov@sap.com>
2024-07-02 16:40:23 +0200

    User fastapi directly instead of uvicorn

--- src/services/chat.py
+++ src/services/chat.py
@@ -1,9 +1,5 @@
-import asyncio
-from collections.abc import AsyncGenerator
-
 from utils.logging import get_logger
 from utils.models import get_model
-from utils.utils import create_ndjson_str
 
 logger = get_logger(__name__)
 
@@ -14,27 +10,11 @@ async def init_chat() -> dict:
     return {"message": "Chat is initialized!"}
 
 
-async def handle_request() -> AsyncGenerator[str, None]:
+async def process_chat_request() -> dict:
     """ Chat with the Kyma companion """
     logger.info("Processing request...")
 
     llm = get_model("gpt-4o")
     logger.info(f"LLM model {llm} is created.")
 
-    # dummy implementation
-    for i in range(8):
-        # return event with status
-        yield create_ndjson_str({
-            "step": "action",
-            "result": "Doing Step \n" + str(i + 1)
-        })
-        max_wait_count = 4
-        if i < max_wait_count:
-            # wait for 1 seconds
-            await asyncio.sleep(1.0)
-
-    # return final response.
-    yield create_ndjson_str({
-        "step": "output",
-        "result": "Completed!}\n"
-    })
+    return {"message": "Hello I am Kyma Companion!"}
