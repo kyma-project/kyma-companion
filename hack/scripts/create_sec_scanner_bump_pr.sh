50b53c66b4af19120d115742c48b737bf4ea85e8
Muhammad Faizan <m.faizan@sap.com>
2024-07-19 11:36:33 +0200

    updated workflow

--- hack/scripts/create_sec_scanner_bump_pr.sh
+++ hack/scripts/create_sec_scanner_bump_pr.sh
@@ -43,7 +43,6 @@ git push --set-upstream origin ${BUMP_SEC_SCANNERS_BRANCH_NAME} -f
 pr_link=$(gh pr create -B main --title "chore: bump sec-scanners-config.yaml to ${TAG}" --body "" | tail -n 1)
 
 pr_number=$(echo "$pr_link" | awk -F'/' '{print $NF}')
-gh pr edit $pr_number --add-label kind/enhancement
 
 # NOTE: make sure that this script only echo the id of the PR.
 # It should not echo any other information e.g. debug logs/status messages.
