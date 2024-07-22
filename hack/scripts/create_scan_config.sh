27fddb4a15b794c64126c609e41587d97f075713
Muhammad Faizan <m.faizan@sap.com>
2024-07-22 16:44:25 +0200

    updated script

--- hack/scripts/create_scan_config.sh
+++ hack/scripts/create_scan_config.sh
@@ -6,14 +6,14 @@
 # ./create_scan_config image temp_scan_config.yaml tag
 # - use when bumping the config on the main branch
 
-FILENAME=${1}
-TAG=${2}
-
 # standard bash error handling
 set -o nounset  # treat unset variables as an error and exit immediately.
 set -o errexit  # exit immediately when a command fails.
 set -E          # needs to be set if we want the ERR trap
 set -o pipefail # prevents errors in a pipeline from being maskedPORT=5001
+
+FILENAME=${1}
+TAG=${2}
 
 echo "Creating security scan configuration file:"
 
