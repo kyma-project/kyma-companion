990a2d4d322881775ff3e5b00399461bc57d1e03
Carina Kothe <carina.kothe@sap.com>
2024-07-22 16:55:34 +0200

    use script to extract python version

---
+++ .github/scripts/extract-python-version.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# This script get the python version from the pyproject.toml file
+
+# Error handling.
+set -o nounset  # treat unset variables as an error and exit immediately.
+set -o errexit  # exit immediately when a command fails.
+set -E          # needs to be set if we want the ERR trap
+set -o pipefail # prevents errors in a pipeline from being masked
+
+echo "Getting Python version from pyproject.toml"
+
+# Extract Python version from pyproject.toml and set it as a GitHub Actions environment variable
+PYTHON_VERSION=$(grep '^python = ' pyproject.toml | sed 's/python = \"//g' | sed 's/\"//g')
+echo "PYTHON_VERSION=${PYTHON_VERSION}" >> "$GITHUB_ENV"
