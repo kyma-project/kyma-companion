d4b76b10997b5da6feb5f3b9d68933f4cdf99b39
Muhammad Faizan <m.faizan@sap.com>
2024-07-24 13:21:57 +0200

    lint fixes

--- scripts/python/wait-for-commit-check/main.py
+++ scripts/python/wait-for-commit-check/main.py
@@ -1,7 +1,8 @@
+import datetime
 import os
 import time
+
 import requests
-import datetime
 
 ##############################################################################
 # NOTE: This script is used in the GitHub Actions workflow.
@@ -10,18 +11,24 @@ import datetime
 
 # This script waits for git check to be completed.
 # There are two types of git statuses i.e. check runs and statuses.
-# For more information, see # https://docs.github.com/en/pull-requests/collaborating-with-pull-requests/collaborating-on-repositories-with-code-quality-features/about-status-checks#types-of-status-checks-on-github
+# For more information, see
+# noqa: E501
+# https://docs.github.com/en/pull-requests/collaborating-with-pull-requests/collaborating-on-repositories-with-code-quality-features/about-status-checks#types-of-status-checks-on-github
 
 # Required env variables:
-# - GITHUB_TOKEN                   - GitHub token for authentication.
-# - REPOSITORY_FULL_NAME           - Repository name including owner name e.g. kyma-project/kyma-companion.
-# - GIT_REF                        - Git reference to check for the check run (i.e. sha, branch name or tag name).
-# - GIT_CHECK_RUN_NAME             - Name of the git check to wait for.
-# - INTERVAL                       - Interval in seconds to wait before check the status again.
-# - TIMEOUT                        - Timeout in seconds to wait for the check run to complete before failing.
+# - GITHUB_TOKEN          - GitHub token for authentication.
+# - REPOSITORY_FULL_NAME  - Repository name including owner name e.g. kyma-project/kyma-companion.
+# - GIT_REF               - Git reference to check for the
+#                           check run (i.e. sha, branch name or tag name).
+# - GIT_CHECK_RUN_NAME    - Name of the git check to wait for.
+# - INTERVAL              - Interval in seconds to wait before check the status again.
+# - TIMEOUT               - Timeout in seconds to wait for the check run to complete before failing.
+
+HTTP_OK = 200
 
 
-def read_inputs():
+def read_inputs() -> dict:
+    """Returns the dict with configs read from the environment variables."""
     github_token = os.environ.get("GITHUB_TOKEN")
     if github_token is None or github_token == '':
         exit('ERROR: Env GITHUB_TOKEN is missing')
@@ -62,7 +69,8 @@ def read_inputs():
     }
 
 
-def print_inputs(inputs):
+def print_inputs(inputs: dict) -> None:
+    """Prints the configurations."""
     print('**** Using the following configurations: ****', flush=True)
     print('Repository Full Name: {}'.format(inputs['repository_full_name']), flush=True)
     print('Git REF : {}'.format(inputs['git_ref']), flush=True)
@@ -71,23 +79,26 @@ def print_inputs(inputs):
     print('Interval : {}'.format(inputs['interval']), flush=True)
 
 
-def fetch_check_runs(repo, git_ref, token):
+def fetch_check_runs(repo: str, git_ref: str, token: str) -> dict:
+    """Fetches the check runs from GitHub."""
     # https://docs.github.com/en/rest/checks/runs?apiVersion=2022-11-28#list-check-runs-for-a-git-reference
-    url = "https://api.github.com/repos/{}/commits/{}/check-runs".format(repo, git_ref)
+    url = f"https://api.github.com/repos/{repo}/commits/{git_ref}/check-runs"
     req_headers = {
         'Accept': 'application/vnd.github+json',
         'X-GitHub-Api-Version': '2022-11-28',
-        'Authorization': 'Bearer {}'.format(token)
+        'Authorization': f'Bearer {token}'
     }
 
-    print('Fetching check runs from {}'.format(url), flush=True)
+    print(f'Fetching check runs from {url}', flush=True)
     response = requests.get(url, headers=req_headers)
-    if response.status_code != 200:
-        raise Exception('API call failed. Status code: {}, {}'.format(response.status_code, response.text))
+
+    if response.status_code != HTTP_OK:
+        raise Exception(f'API call failed. Status code: {response.status_code}, {response.text}')
     return response.json()
 
 
-def get_latest_check_run(check_run_name, check_runs):
+def get_latest_check_run(check_run_name: str, check_runs: dict) -> dict:
+    """Returns the latest check run by name."""
     result = None
     latest_start_time = None
     for run in check_runs:
@@ -100,35 +111,41 @@ def get_latest_check_run(check_run_name, check_runs):
     return result
 
 
-def main():
+def main() -> None:
+    """Main"""
     inputs = read_inputs()
     print_inputs(inputs)
 
     start_time = time.time() # seconds
     while True:
-        print('****************************************************************************************', flush=True)
+        print('********************************************'
+              '********************************************', flush=True)
         # Sleep for `interval`.
         # sleeping before first check, so that any pending workflow on Git ref is triggered/updated.
         time.sleep(inputs['interval'])
 
         # check if timeout has reached.
         elapsed_time = time.time() - start_time
-        print('Elapsed time: {} secs (timout: {} secs)'.format(elapsed_time, inputs['timeout']), flush=True)
+        print('Elapsed time: {} secs (timout: {} secs)'.format(
+            elapsed_time, inputs['timeout']), flush=True)
         if elapsed_time > inputs['timeout']:
             print('Error: Timed out!', flush=True)
             exit(1)
 
         # fetch check runs from GitHub.
-        check_runs = fetch_check_runs(inputs['repository_full_name'], inputs['git_ref'], inputs['token'])
+        check_runs = fetch_check_runs(inputs['repository_full_name'],
+                                      inputs['git_ref'], inputs['token'])
 
         # extract the latest check run (because there may be multiple runs by same name).
-        latest_check_run = get_latest_check_run(inputs['git_check_run_name'], check_runs['check_runs'])
+        latest_check_run = get_latest_check_run(inputs['git_check_run_name'],
+                                                check_runs['check_runs'])
         if latest_check_run is None:
             print('Check run not found. Waiting...', flush=True)
             continue
 
         # print details of the latest check run.
-        print('Found Check run: {} ({})'.format(latest_check_run['name'], latest_check_run['html_url']), flush=True)
+        print('Found Check run: {} ({})'.format(latest_check_run['name'],
+                                                latest_check_run['html_url']), flush=True)
         print('Check run Head-SHA: {}'.format(latest_check_run['head_sha']), flush=True)
         print('Check run start-at: {}'.format(latest_check_run['started_at']), flush=True)
         print('Check run status: {}'.format(latest_check_run['status']), flush=True)
@@ -143,7 +160,8 @@ def main():
             exit(0)
 
         # https://docs.github.com/en/rest/checks/runs?apiVersion=2022-11-28#list-check-runs-for-a-git-reference
-        if latest_check_run['conclusion'] in ['failure', 'neutral', 'cancelled', 'skipped', 'timed_out']:
+        if latest_check_run['conclusion'] in ['failure', 'neutral',
+                                              'cancelled', 'skipped', 'timed_out']:
             print('Check run completed with failure.', flush=True)
             exit(1)
 
