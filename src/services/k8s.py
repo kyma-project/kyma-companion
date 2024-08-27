e21b775f64349107c1f9ecd012b3f377da78e636
Muhammad Faizan <m.faizan@sap.com>
2024-08-27 16:28:41 +0200

    added methods in k8sClient

--- src/services/k8s.py
+++ src/services/k8s.py
@@ -1,5 +1,6 @@
 import base64
 import os
+import copy
 import tempfile
 from abc import ABC, abstractmethod
 
@@ -45,6 +46,7 @@ class K8sClient(K8sClientInterface):
     dynamic_client: dynamic.DynamicClient = None
 
     def __init__(self, api_server: str, user_token: str, certificate_authority_data: str):
+        """Initialize the K8sClient object."""
         self.api_server = api_server
         self.user_token = user_token
         self.certificate_authority_data = certificate_authority_data
@@ -58,15 +60,19 @@ class K8sClient(K8sClientInterface):
         self.dynamic_client = self._create_dynamic_client()
 
     def __del__(self):
+        """Destructor to remove the temporary file containing certificate authority data."""
         if self.ca_temp_filename != "":
-            # TODO: check which one is correct
-            os.remove(self.ca_temp_filename)
-            # os.unlink(self.ca_temp_filename)
+            try:
+                os.remove(self.ca_temp_filename)
+            except FileNotFoundError:
+                pass
 
     def _get_decoded_ca_data(self) -> str:
+        """Decode the certificate authority data."""
         return base64.b64decode(self.certificate_authority_data)
 
     def _create_dynamic_client(self) -> dynamic.DynamicClient:
+        """Create a dynamic client for the K8s API."""
         # Create configuration object for client.
         conf = client.Configuration()
         conf.host = self.api_server
@@ -79,39 +85,60 @@ class K8sClient(K8sClientInterface):
             client.api_client.ApiClient(configuration=conf)
         )
 
-    def execute_get_api_request(self, uri: str) -> dict:
-        # define headers for the API request
-        headers = {
+    def _get_auth_headers(self) -> dict:
+        """Get the authentication headers for the Kubernetes API request."""
+        return {
             "Authorization": "Bearer " + self.user_token,
             "Accept": "application/json",
             "Content-Type": "application/json"
         }
 
-        # Make the API request
+    def execute_get_api_request(self, uri: str) -> dict:
+        """Execute a GET request to the Kubernetes API."""
         response = requests.get(
             url=f"{self.api_server}/{uri.lstrip('/')}",
-            headers=headers,
+            headers=self._get_auth_headers(),
             verify=self.ca_temp_filename
         )
 
         return response.json()
 
     def list_resource(self, api_version: str, kind: str, namespace: str) -> list:
+        """List resources of a specific kind in a namespace.
+        Provide empty string for namespace to list resources in all namespaces."""
         result = self.dynamic_client.resources.get(api_version=api_version, kind=kind).get(namespace=namespace)
         return result.items
 
     def get_resource(self, api_version: str, kind: str, name: str, namespace: str) -> dict:
+        """Get a specific resource by name in a namespace."""
         return self.dynamic_client.resources.get(api_version=api_version, kind=kind).get(name=name, namespace=namespace)
 
+    def describe_resource(self, api_version: str, kind: str, name:str, namespace: str) -> list:
+        """Describe a specific resource by name in a namespace. This includes the resource and its events."""
+        resource = self.get_resource(api_version, kind, name, namespace)
+
+        # clone the object because we cannot modify the original object.
+        result = copy.deepcopy(resource.to_dict())
+
+        # get events for the resource.
+        result["events"] = self.list_k8s_events_for_resource(kind, name, namespace)
+        for event in result["events"]:
+            del event["involvedObject"]
+        return result
+
     def list_not_running_pods(self, namespace: str) -> list:
+        """List all pods that are not in the Running phase.
+        Provide empty string for namespace to list all pods."""
         all_pods = self.list_resource("v1", "Pod", namespace)
         return [pod for pod in all_pods if pod.status.phase != "Running"]
 
     def list_nodes_metrics(self) -> list:
+        """List all nodes metrics."""
         result = self.execute_get_api_request("apis/metrics.k8s.io/v1beta1/nodes")
         return list(result["items"])
 
     def list_k8s_events(self, namespace: str) -> list:
+        """List all Kubernetes events. Provide empty string for namespace to list all events."""
         uri = "api/v1/events?limit=500"
         if namespace != "":
             uri = f"api/v1/namespaces/{namespace}/events?limit=500"
@@ -119,4 +146,15 @@ class K8sClient(K8sClientInterface):
         return list(result["items"])
 
     def list_k8s_warning_events(self, namespace: str) -> list:
+        """List all Kubernetes warning events. Provide empty string for namespace to list all warning events."""
         return [event for event in self.list_k8s_events(namespace) if event["type"] == "Warning"]
+
+    def list_k8s_events_for_resource(self, kind: str, name: str, namespace: str) -> list:
+        """List all Kubernetes events for a specific resource. Provide empty string for namespace to list all events."""
+        events = self.list_k8s_events(namespace)
+        result = []
+        for event in events:
+            if event["involvedObject"]["kind"] == kind and event["involvedObject"]["name"] == name:
+                result.append(event)
+
+        return result
