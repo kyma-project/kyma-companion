a936032fcb3d069ae0fd0ff16e360c53430108af
Friedrich Wilken <jens.wilken@sap.com>
2024-09-05 18:35:05 +0200

    add test for new_conversation

--- tests/services/test_conversation.py
+++ tests/services/test_conversation.py
@@ -9,57 +9,102 @@ from services.conversation import ConversationService
 @pytest.mark.asyncio(scope="class")
 class TestConversation:
 
-    @pytest.fixture
-    def mock_model_factory(self):
-        mock_model = Mock()
-        with patch("services.conversation.ModelFactory") as mock:
-            mock.return_value.create_model.return_value = mock_model
-            yield mock
+	@pytest.fixture
+	def mock_model_factory(self):
+		mock_model = Mock()
+		with patch("services.conversation.ModelFactory") as mock:
+			mock.return_value.create_model.return_value = mock_model
+			yield mock
 
-    @pytest.fixture
-    def mock_kyma_graph(self):
-        mock_kyma_graph = MagicMock()
-        mock_kyma_graph.astream.return_value = AsyncMock()
-        mock_kyma_graph.astream.return_value.__aiter__.return_value = [
-            "chunk1",
-            "chunk2",
-            "chunk3",
-        ]
-        with patch(
-            "services.conversation.KymaGraph", return_value=mock_kyma_graph
-        ) as mock:
-            yield mock
+	@pytest.fixture
+	def mock_kyma_graph(self):
+		mock_kyma_graph = MagicMock()
+		mock_kyma_graph.astream.return_value = AsyncMock()
+		mock_kyma_graph.astream.return_value.__aiter__.return_value = [
+			"chunk1",
+			"chunk2",
+			"chunk3",
+		]
+		with patch(
+			"services.conversation.KymaGraph", return_value=mock_kyma_graph
+		) as mock:
+			yield mock
+	
+	@pytest.fixture
+	def mock_redis_saver(self):
+		async def async_mock_add_conversation_message(*args, **kwargs):
+			pass  # This method does not return anything
 
-    @pytest.fixture
-    def mock_redis_saver(self):
-        with patch("services.conversation.RedisSaver") as mock:
-            yield mock
+		with patch("services.conversation.RedisSaver") as mock:
+			mock.return_value.add_conversation_message = AsyncMock(side_effect=async_mock_add_conversation_message)
+			yield mock
 
-    @pytest.fixture
-    def mock_init_pool(self):
-        with patch("services.conversation.initialize_async_pool") as mock:
-            yield mock
+	@pytest.fixture
+	def mock_init_pool(self):
+		with patch("services.conversation.initialize_async_pool") as mock:
+			yield mock
 
-    @pytest.mark.asyncio
-    async def test_init_chat(
-        self, mock_model_factory, mock_init_pool, mock_redis_saver, mock_kyma_graph
-    ):
-        messaging_service = ConversationService()
-        result = messaging_service.init_conversation()
-        assert result == {"message": "Chat is initialized!"}
+	@pytest.fixture
+	def mock_generate_initial_questions(self):
+		async def async_mock(*args, **kwargs):
+			return ["question1?", "question2?", "question3?"]
+		
+		with patch.object(
+				ConversationService,
+				"generate_initial_questions",
+				new=AsyncMock(side_effect=async_mock),
+			) as mock:
+			yield mock
 
-    @pytest.mark.asyncio
-    async def test_handle_request(
-        self, mock_model_factory, mock_init_pool, mock_redis_saver, mock_kyma_graph
-    ):
-        messaging_service = ConversationService()
+	@pytest.mark.asyncio
+	async def test_new_conversation(
+		self,
+		mock_model_factory,
+		mock_init_pool,
+		mock_redis_saver,
+		mock_kyma_graph,
+		mock_generate_initial_questions,
+	):
+		# Arrange:
+		test_conversation_id = 1
+		test_message = Message(
+				query="test query",
+				resource_kind="Pod",
+				resource_api_version="v1",
+				resource_name="my-pod",
+				namespace="default",
+			)
+		test_k8s_client = MagicMock()
 
-        message = Message(
-            query="Test message",
-            resource_kind="Pod",
-            resource_api_version="v1",
-            resource_name="my-pod",
-            namespace="default",
-        )
-        result = [chunk async for chunk in messaging_service.handle_request(1, message)]
-        assert result == [b"chunk1", b"chunk2", b"chunk3"]
+		# Act:
+		messaging_service = ConversationService()
+		result = await messaging_service.new_conversation(
+			conversation_id=test_conversation_id,
+			message=test_message,
+			k8s_client=test_k8s_client
+		)
+
+		# Assert:
+		mock_generate_initial_questions.assert_called_once_with(
+			conversation_id=test_conversation_id,
+			message=test_message,
+			k8s_client=test_k8s_client
+		)
+		mock_redis_saver.return_value.add_conversation_message.assert_called_once()
+		assert result == ["question1?", "question2?", "question3?"]
+
+	@pytest.mark.asyncio
+	async def test_handle_request(
+		self, mock_model_factory, mock_init_pool, mock_redis_saver, mock_kyma_graph
+	):
+		messaging_service = ConversationService()
+
+		message = Message(
+			query="Test message",
+			resource_kind="Pod",
+			resource_api_version="v1",
+			resource_name="my-pod",
+			namespace="default",
+		)
+		result = [chunk async for chunk in messaging_service.handle_request(1, message)]
+		assert result == [b"chunk1", b"chunk2", b"chunk3"]
