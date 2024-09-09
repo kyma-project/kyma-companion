3a32ca8929c30381abb87381b74b71e543d0f5db
Carina Kothe <carina.kothe@sap.com>
2024-09-09 23:16:11 +0200

    add scenarios and validation

---
+++ tests/blackbox/data/evaluation/namespace-scoped/bitnami_no_rbac_permissions/deploy.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "## Deploying no-rbac-permissions scenario ##"
+kubectl apply -f deployment.yaml
---
+++ tests/blackbox/data/evaluation/namespace-scoped/bitnami_wrong_rbac_permissions/deploy.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "## Deploying wrong-rbac-permissions scenario ##"
+kubectl apply -f deployment.yaml
