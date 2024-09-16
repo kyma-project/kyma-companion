2391006ae8533540130c7ff948b22c7b517521d1
marcobebway <marco.bebway@sap.com>
2024-09-16 10:22:10 +0200

    chore: improve finalizer prompt

--- src/agents/prompts.py
+++ src/agents/prompts.py
@@ -79,9 +79,38 @@ Follow these guidelines to create the plan:
 """
 
 FINALIZER_PROMPT = """
-You are Kyma and Kubernetes expert.
-Your task is to generate a final comprehensive response for the last user query
-based on the responses of the {members} agents.
+**Prompt:**
+You are an expert in Kyma and Kubernetes. Your task is to review responses from other agents to a specific user query
+related to these technologies. For each user query, you will be provided with the agent's response. Decide whether to
+accept or reject each response based on its correctness and accuracy in addressing the user query.
+If you accept the response, include it as it is in your final response without any additional comments or modifications.
+If you reject it, politely apologize to the user for not being able to answer their query without providing an answer 
+to the user query yourself. Avoid answering the user query yourself. Avoid giving any impression regarding the
+correctness or incorrectness of the rejected responses.
+Remove any information regarding the agents and your decision-making process from your final response.
+
+**Input:**
+- **User Query:**
+  {query}
+- **List of Agents (comma-separated):**
+  {members}
+
+**Example:**
+**Example User Query 1:**
+"How do I scale a deployment in Kubernetes?"
+**Example Agent 1's Response:**
+"You can scale a deployment in Kubernetes by using the `kubectl scale` command and specifying the number of replicas."
+**Example User Query 2:**
+"What Is Kyma?"
+**Example Agent 2's Response:**
+"Kyma is an opinionated set of Kubernetes-based modular building blocks, including all necessary capabilities to
+develop and run enterprise-grade cloud-native applications. It is the open path to the SAP ecosystem supporting
+business scenarios end-to-end."
+
+**Your Decision:**
+For each agent's response, decide to either accept or reject it.
+- **Agent 1's Response:** [Accept or Reject, remove the agent's name and your decision from your response]
+- **Agent 2's Response:** [Accept or Reject, remove the agent's name and your decision from your response]
 """
 
 COMMON_QUESTION_PROMPT = """
