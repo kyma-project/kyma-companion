73199f55f25b4db109233db607aca29e7be6ea22
Friedrich Wilken <jens.wilken@sap.com>
2024-09-19 21:13:15 +0200

    simplify initial questions

--- src/initial_questions/inital_questions.py
+++ src/initial_questions/inital_questions.py
@@ -58,6 +58,11 @@ class InitialQuestionsHandler:
 
         logger.info("Fetching relevant data from k8s cluster")
 
+        namespace: str = message.namespace or ""
+        kind: str = message.resource_kind or ""
+        name: str = message.resource_name or ""
+        api_version: str = message.resource_api_version or ""
+
         # Query the Kubernetes API to get the context.
         context = ""
 
@@ -66,54 +71,48 @@ class InitialQuestionsHandler:
         # by fetching all not running pods,
         # all K8s Nodes metrics,
         # and all K8s events with warning type.
-        if (
-            is_empty_str(message.namespace)
-            and str(message.resource_kind).lower() == "cluster"
-        ):
+        if is_empty_str(namespace) and kind.lower() == "cluster":
             logger.info(
                 "Fetching all not running Pods, Node metrics, and K8s Events with warning type"
             )
-            pods = yaml.dump_all(k8s_client.list_not_running_pods(namespace=""))
+            pods = yaml.dump_all(k8s_client.list_not_running_pods(namespace=namespace))
             metrics = yaml.dump_all(k8s_client.list_nodes_metrics())
-            events = yaml.dump_all(k8s_client.list_k8s_warning_events(namespace=""))
+            events = yaml.dump_all(
+                k8s_client.list_k8s_warning_events(namespace=namespace)
+            )
 
             context = f"{pods}\n{metrics}\n{events}"
 
         # If the namespace is provided, and the resource Kind is 'namespace'
         # get an overview of the namespace
         # by fetching all K8s events with warning type.
-        elif (
-            is_non_empty_str(message.namespace)
-            and str(message.resource_kind).lower() == "namespace"
-        ):
+        elif is_non_empty_str(namespace) and kind.lower() == "namespace":
             logger.info("Fetching all K8s Events with warning type")
             context = yaml.dump_all(
-                k8s_client.list_k8s_warning_events(namespace=str(message.namespace))
+                k8s_client.list_k8s_warning_events(namespace=namespace)
             )
 
         # If the namespace is not provided, but the resource Kind is
         # describe that resource.
         # If the namespace is empty, query not-namespaced resources.
         # Finally, get all events related to given resource.
-        elif is_non_empty_str(message.resource_kind) and is_non_empty_str(
-            message.resource_api_version
-        ):
+        elif is_non_empty_str(kind) and is_non_empty_str(api_version):
             logger.info(
-                f"Fetching all entities of Kind {message.resource_kind} with API version {message.resource_api_version}"
+                f"Fetching all entities of Kind {kind} with API version {api_version}"
             )
             resources = yaml.dump(
                 k8s_client.describe_resource(
-                    api_version=str(message.resource_api_version),
-                    kind=str(message.resource_kind),
-                    name=str(message.resource_name),
-                    namespace=message.namespace if message.namespace else "",
+                    api_version=api_version,
+                    kind=kind,
+                    name=name,
+                    namespace=namespace,
                 )
             )
             events = yaml.dump_all(
                 k8s_client.list_k8s_events_for_resource(
-                    kind=str(message.resource_kind),
-                    name=str(message.resource_name),
-                    namespace=message.namespace if message.namespace else "",
+                    kind=kind,
+                    name=name,
+                    namespace=namespace,
                 )
             )
 
