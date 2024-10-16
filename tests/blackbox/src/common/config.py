6e2c875c820a94078d0e764cd4728d4eee3742f7
Friedrich Wilken <jens.wilken@sap.com>
2024-10-16 21:24:01 +0200

    remove unused var in config

--- tests/blackbox/src/common/config.py
+++ tests/blackbox/src/common/config.py
@@ -13,14 +13,10 @@ class Config:
     test_data_path: str  # Path to the test data directory e.g. "~kyma-companion/tests/blackbox/data"
     namespace_scoped_test_data_path: str
     companion_api_url: str
-    companion_token: (
-        str  # Authentication token when the companion is deployed in MPS cluster.
-    )
+    companion_token: str  # Authentication token when the companion is deployed in MPS cluster.
     test_cluster_url: str  # Gardener test cluster API server URL.
     test_cluster_ca_data: str  # Gardener test cluster CA data.
     test_cluster_auth_token: str  # Gardener test cluster authentication token.
-    aicore_deployment_id_gpt4: str
-    aicore_configuration_id_gpt4: str
     aicore_deployment_id_gpt4_mini: str
     aicore_configuration_id_gpt4_mini: str
     model_name: str
@@ -39,10 +35,8 @@ class Config:
         self.test_cluster_url = get_env("TEST_CLUSTER_URL")
         self.test_cluster_ca_data = get_env("TEST_CLUSTER_CA_DATA")
         self.test_cluster_auth_token = get_env("TEST_CLUSTER_AUTH_TOKEN")
-        self.aicore_deployment_id_gpt4 = get_env("AICORE_DEPLOYMENT_ID_GPT4")
-        self.aicore_configuration_id_gpt4 = get_env("AICORE_CONFIGURATION_ID_GPT4")
         self.aicore_deployment_id_gpt4_mini = get_env("AICORE_DEPLOYMENT_ID_GPT4")
-        self.aicore_configuration_id_gpt4_min = get_env("AICORE_CONFIGURATION_ID_GPT4")
+        self.aicore_configuration_id_gpt4_mini = get_env("AICORE_CONFIGURATION_ID_GPT4")
         self.model_name = get_env("MODEL_NAME", False, "gpt4.o-mini")
         self.model_temperature = 0
         self.iterations = 3
