a7a22028384e35c8dd33846b4ef857c7c8e238e0
marcobebway <marco.bebway@sap.com>
2024-10-24 13:31:09 +0200

    chore: enable/disable langfuse using config

--- src/utils/langfuse.py
+++ src/utils/langfuse.py
@@ -1,9 +1,11 @@
 from langfuse.callback import CallbackHandler
 
-from utils.settings import LANGFUSE_HOST, LANGFUSE_PUBLIC_KEY, LANGFUSE_SECRET_KEY
+from utils.utils import string_to_bool
+from utils.settings import LANGFUSE_HOST, LANGFUSE_PUBLIC_KEY, LANGFUSE_SECRET_KEY, LANGFUSE_ENABLED
 
 handler = CallbackHandler(
     secret_key=LANGFUSE_SECRET_KEY,
     public_key=LANGFUSE_PUBLIC_KEY,
     host=LANGFUSE_HOST,
+    enabled=string_to_bool(LANGFUSE_ENABLED),
 )
--- src/utils/utils.py
+++ src/utils/utils.py
@@ -31,3 +31,12 @@ def is_empty_str(data: str | None) -> bool:
 def is_non_empty_str(data: str | None) -> bool:
     """Check if the string is not None and not empty."""
     return data is not None and data.strip() != ""
+
+
+def string_to_bool(value: str) -> bool:
+    """Convert a string to a boolean value."""
+    if value.lower() in ["true", "1", "t", "y", "yes"]:
+        return True
+    if value.lower() in ["false", "0", "f", "n", "no"]:
+        return False
+    raise ValueError(f"{value} is not a valid boolean value.")
