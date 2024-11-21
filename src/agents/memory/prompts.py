e64ff9ae06c6f8429637cac7ad0c426bc06bc691
Muhammad Faizan <m.faizan@sap.com>
2024-11-21 18:52:03 +0500

    updated

--- src/agents/memory/prompts.py
+++ src/agents/memory/prompts.py
@@ -1,7 +1,11 @@
 MESSAGES_SUMMARY_PROMPT = """
-Summarize the following conversation to be used as context to LLM calls. 
-Give more priority to the latest messages.
-Focus on the main issues and solutions proposed. 
-If multiple topics are discussed in the conversation, then summarize each topic separately.
-Keep the summary clear and concise.
+Your task is to summarize the given conversation. 
+Your output will be used as context for other LLM agents to respond to user queries. 
+Therefore, summarize in such a way the is most useful for other agents to understand the conversation.
+
+Instructions:
+- Give more priority to the latest messages in the conversation.
+- Focus on the technical issues discussed and the solutions proposed.
+- If multiple topics are discussed, summarize each topic separately.
+- Keep the summary clear and concise.
 """
