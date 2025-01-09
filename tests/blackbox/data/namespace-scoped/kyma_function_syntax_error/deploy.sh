4650527f021cb32bdc42a8bb42e8f4ae117acf4d
marcobebway <marco.bebway@sap.com>
2025-01-09 10:00:12 +0100

    chore: add blackbox tests for Kyma

---
+++ tests/blackbox/data/namespace-scoped/kyma_function_syntax_error/deploy.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "## Deploy kyma_function_syntax_error scenario ##"
+kubectl apply -f deployment.yml
---
+++ tests/blackbox/data/namespace-scoped/kyma_subscription_invalid_sink/deploy.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "## Deploy kyma_subscription_invalid_sink scenario ##"
+kubectl apply -f deployment.yml
