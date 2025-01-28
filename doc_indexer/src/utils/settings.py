e885aaa052158f7f396686d8d4aacee13f3b0049
Muhammad Faizan <m.faizan@sap.com>
2025-01-28 10:13:07 +0100

    updated doc_indexer to use config file

--- doc_indexer/src/utils/settings.py
+++ doc_indexer/src/utils/settings.py
@@ -1,36 +1,56 @@
+import json
 import logging
 import os
-import sys
+from pathlib import Path
 
-from decouple import Config, RepositoryEnv, config
-from dotenv import find_dotenv, load_dotenv
+from decouple import config
 
-
-def is_running_pytest() -> bool:
-    """Check if the code is running with pytest.
-    This is needed to identify if tests are running.
-    """
-    return "pytest" in sys.modules
-
+from utils.models import ModelConfig
 
 project_root = os.path.dirname(os.path.abspath(__file__))
 
-if is_running_pytest():
-    # Use .test.env for tests
-    env_path = find_dotenv(".env.test")
-    if env_path and os.path.exists(env_path):
-        config = Config(RepositoryEnv(env_path))
-        load_dotenv(env_path)
-    else:
-        logging.warning("No .test.env file found. Using .env file.")
-        load_dotenv()
-else:
-    load_dotenv()
+MODELS_CONFIGS_KEY = "MODELS_CONFIGS"
+
+
+def load_env_from_json() -> None:
+    """Load the configuration from the config.json file."""
+    default_config_path = (
+        Path(__file__).parent.parent.parent.parent / "config" / "config.json"
+    )
+
+    config_path = Path(os.getenv("CONFIG_PATH", default_config_path))
+
+    try:
+        # Load the configuration from the given path and set the environment variables.
+        with config_path.open() as file:
+            config_file = json.load(file)
+
+            # Set environment variables for all keys except "models"
+            for key, value in config_file.items():
+                if key != "models":
+                    os.environ[key] = str(value)
+                else:
+                    os.environ[MODELS_CONFIGS_KEY] = json.dumps(value)
+    except json.JSONDecodeError as e:
+        logging.error(f"Invalid JSON format in config file {config_path}: {e}")
+        raise
+    except FileNotFoundError:
+        logging.error(
+            f"Config file not found at {config_path}. Place the config file at the default location:"
+            f"{default_config_path} or set the CONFIG_PATH environment variable."
+        )
+        raise
+    except Exception as e:
+        logging.error(f"Error loading config from {config_path}: {e}")
+        raise
+
+
+# Load the environment variables from the json file.
+load_env_from_json()
 
 
 LOG_LEVEL = config("LOG_LEVEL", default="INFO")
-EMBEDDING_MODEL_DEPLOYMENT_ID = config("EMBEDDING_MODEL_DEPLOYMENT_ID")
-EMBEDDING_MODEL_NAME = config("EMBEDDING_MODEL_NAME")
+EMBEDDING_MODEL_NAME = config("EMBEDDING_MODEL_NAME", default="text-embedding-3-large")
 
 TMP_DIR = config("TMP_DIR", default=os.path.join(project_root, "tmp"))
 DOCS_SOURCES_FILE_PATH = config(
@@ -46,3 +66,14 @@ DATABASE_USER = config("DATABASE_USER")
 DATABASE_PASSWORD = config("DATABASE_PASSWORD")
 
 INDEX_TO_FILE = config("INDEX_TO_FILE", default=False)
+
+
+def get_embedding_model_config(name: str) -> ModelConfig:
+    """Get the configuration of the embedding model by name."""
+    models = config(MODELS_CONFIGS_KEY, cast=json.loads)
+    for model in models:
+        if model["name"] == name:
+            # cast model as ModelConfig
+            return ModelConfig(**model)
+
+    raise ValueError(f"Model {name} not found in the configuration.")
