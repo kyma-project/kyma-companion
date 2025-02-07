285240a020caa4f5308ba689965551c3708ce161
Friedrich Wilken <jens.wilken@sap.com>
2025-02-07 11:05:22 +0100

    Revert "add restart"
    This reverts commit 8e16d2b13d92be9b3b8fadbeb15b3318026d5704.

--- src/main.py
+++ src/main.py
@@ -28,9 +28,4 @@ async def healthz() -> dict:
 
 
 if __name__ == "__main__":
-    uvicorn.run(
-        "main:app",
-        host="localhost",
-        port=8000,
-        reload=True,
-    )
+    uvicorn.run(app, host="localhost", port=8000)
