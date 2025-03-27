4011bcdce8b5dcc6ce6caf3d8a88c314688dcb30
Friedrich Wilken <jens.wilken@sap.com>
2025-03-27 17:27:56 +0100

    use the test case as an assert argument

--- doc_indexer/tests/unit/indexing/test_indexer.py
+++ doc_indexer/tests/unit/indexing/test_indexer.py
@@ -280,6 +280,6 @@ def test_index(
                 mock_sleep.assert_has_calls([call(3)] * (len(expected_batches) - 1))
 
     if delete_error:
-        assert str(delete_error) in str(indexer.db.delete.side_effect)
+        assert str(delete_error) in str(indexer.db.delete.side_effect), test_case
     if add_error:
-        assert str(add_error) in str(indexer.db.add_documents.side_effect)
+        assert str(add_error) in str(indexer.db.add_documents.side_effect), test_case
