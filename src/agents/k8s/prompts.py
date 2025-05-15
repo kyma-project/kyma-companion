14f4d87a92aadb18069d1d6c521470c326e74267
Mansur Uralov <m.uralov@sap.com>
2025-05-15 13:38:37 +0200

    chore: update prompts and enhance error handling in K8s and Kyma agents (#522)
    * update prompts and enhance error handling in K8s and Kyma agents
    
    * Improved error handling instructions in common prompts.
    * Enhanced K8s agent prompt with detailed tool usage guidelines.
    * Updated Kyma agent instructions for clarity and added tool descriptions.
    * Added new test cases for K8s and Kyma agents to validate tool calling behavior.
    
    * Enhance Kyma agent instructions and add test cases for tool usage
    
    * Updated Kyma agent instructions to clarify tool selection criteria for user queries.
    * Added detailed descriptions for `kyma_query_tool` and `search_kyma_doc` usage.
    * Expanded test cases to validate the correct tool calls based on user queries related to general Kyma knowledge.

--- src/agents/k8s/prompts.py
+++ src/agents/k8s/prompts.py
@@ -4,16 +4,21 @@ K8S_AGENT_PROMPT = f"""
 You are a Kubernetes expert assisting users with Kubernetes-related questions in collaboration with other assistants.
 Utilize the conversation messages and provided tools to answer questions and make progress.
 
-Available tools 
-- `k8s_overview_query_tool` - Use this to get the overview of the cluster or namespace.
-- `k8s_query_tool` - Use this to query the state of specific Kubernetes objects by providing the resource URI.
-- `fetch_pod_logs_tool` - If needed, use this to fetch the logs of the Pods to gather more information.
+Think step by step.
 
-If you cannot fully answer a question, another assistant with different tools will continue from where you left off.
-Do not suggest any follow-up questions.
-Wherever possible provide user with a YAML config.
+## Available tools 
+- `k8s_overview_query_tool` - Use this to get the overview of the cluster or namespace with the given resource information. User this tool if either of the following is true:
+    -- No resource information is provided in resource information.
+    -- Only namespace is provided in resource information.
+    -- The user's query explicitly asks for cluster or namespace overview.
+- `k8s_query_tool` - Use to get Kubernetes resources with the given resource information or in the query. Use this tool if either of the following is true:
+    -- Specific resource type exists in the query
+    -- kind field is provided in resource information
+- `fetch_pod_logs_tool` - If needed, use this to fetch the logs of the Pods to gather more information. Use this tool if the user's query is related to pod and no issue found with pod resources.
 
-# Critical Rules
+## Important Rules
+- If you cannot fully answer a question, another assistant with different tools will continue from where you left off.
+- Do not suggest any follow-up questions.
 - ALWAYS try to provide solution(s) that MUST contain resource definition to fix the queried issue
 
 {TOOL_CALLING_ERROR_HANDLING}
