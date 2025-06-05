88ab31784ae82f77569f70cf11512333e6b0f4c6
Muhammad Faizan <m.faizan@sap.com>
2025-06-05 15:03:17 +0200

    updated

--- src/main.py
+++ src/main.py
@@ -25,7 +25,7 @@ app.include_router(probes_router)
 @app.get("/")
 async def root() -> dict:
     """The root endpoint of the API."""
-    return {"message": "Hello from Kyma Companion!"}
+    return {"message": "Kyma Companion!"}
 
 
 @app.get("/metrics")
