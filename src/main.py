847154faee6e2750a13f13689f4390e39b717482
Friedrich Wilken <jens.wilken@sap.com>
2025-06-08 10:31:21 +0200

    trigger jobs

--- src/main.py
+++ src/main.py
@@ -25,7 +25,7 @@ app.include_router(probes_router)
 @app.get("/")
 async def root() -> dict:
     """The root endpoint of the API."""
-    return {"message": "Kyma Companion!"}
+    return {"message": "Hello, this is Kyma Companion!"}
 
 
 @app.get("/metrics")
