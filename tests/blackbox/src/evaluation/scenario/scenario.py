283b53e75f01f3061170641adfde16856d761f10
Friedrich Wilken <jens.wilken@sap.com>
2025-06-11 15:55:14 +0200

    make fields optional

--- tests/blackbox/src/evaluation/scenario/scenario.py
+++ tests/blackbox/src/evaluation/scenario/scenario.py
@@ -77,10 +77,7 @@ class Query(BaseModel):
         if result.metrics_data is None:
             return False
         for test_metric in result.metrics_data:
-            if (
-                test_metric.name.startswith(REQUIRED_METRIC_PREFIX)
-                and not test_metric.success
-            ):
+            if test_metric.name.startswith(REQUIRED_METRIC_PREFIX) and not test_metric.success:
                 return False
         return True
 
@@ -134,9 +131,7 @@ class ScenarioList(BaseModel):
                 with open(scenario_file) as file:
                     scenario_yaml = yaml.load(file, Loader=yaml.FullLoader)
             except Exception as exception:
-                raise Exception(
-                    f"Error reading scenario file: {scenario_file}"
-                ) from exception
+                raise Exception(f"Error reading scenario file: {scenario_file}") from exception
 
             try:
                 json_str = json.dumps(scenario_yaml)
@@ -145,9 +140,7 @@ class ScenarioList(BaseModel):
                 # add the scenario to the list.
                 self.add(scenario)
             except Exception as exception:
-                raise Exception(
-                    f"Error parsing scenario file: {scenario_file}"
-                ) from exception
+                raise Exception(f"Error parsing scenario file: {scenario_file}") from exception
 
         logger.info(f"Total scenarios loaded: {len(self.items)}")
 
