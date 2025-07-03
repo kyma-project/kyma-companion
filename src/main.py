03ad19b96c75e8817db1a7eb915f803b8feaf2a4
Tanweer <tanweer.salah@gmail.com>
2025-07-03 15:44:55 +0200

    updated exception

--- src/main.py
+++ src/main.py
@@ -30,7 +30,7 @@ async def exception_handler_middleware(req: Request, call_next: Any) -> Any:
         return await call_next(req)
 
     except HTTPException as http_exc:
-        logger.exception(f"HTTPException {http_exc.status_code}: {http_exc}")
+        logger.exception(f"HTTPException {http_exc.status_code}")
         return handle_http_exception(http_exc)
 
     except Exception:
